130f45cb4287a24c86b956c354474845566e60cf
Andreas Rovik <52315788+asc3ndr@users.noreply.github.com>
2020-09-06 21:25:10 +0200

    prep for publish

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function (message, sender) {
-    if (!message.myPopupIsOpen) return;
-
-    alert('works');
-});
--- browser-action/popup.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("clicked-btn").addEventListener("click", function (e) {
-    chrome.runtime.sendMessage({ 'myPopupIsOpen': true });
-});
